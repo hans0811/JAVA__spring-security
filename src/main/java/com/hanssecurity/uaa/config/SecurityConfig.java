3409aa3c6e00d5427ab4f34fcca3a5672244ff10
hans0811 <chaimarket0811@gmail.com>
2021-09-26 01:47:08 +0800

    2 config

--- src/main/java/com/hanssecurity/uaa/config/SecurityConfig.java
+++ src/main/java/com/hanssecurity/uaa/config/SecurityConfig.java
@@ -8,8 +8,10 @@ import lombok.val;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,6 +21,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
@@ -27,6 +30,7 @@ import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 import java.util.Map;
@@ -40,94 +44,102 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Slf4j
 @EnableWebSecurity(debug = true)
 @Import(SecurityProblemSupport.class)
+@Order(99)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final ObjectMapper objectMapper;
     private final SecurityProblemSupport securityProblemSupport;
 
-    // rewrite configure
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        //super.configure(http);
-//        http
-//                .formLogin(Customizer.withDefaults())
-//                .authorizeRequests(req -> req.mvcMatchers("/api/greeting").authenticated());
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/api/**").hasRole("USER")
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().loginPage("/login").usernameParameter("username1")
-//                .and()
-//                .httpBasic().realmName("BA");
-
         http
-                .exceptionHandling(exp-> exp
+                .requestMatchers(req -> req.mvcMatchers("/api/**", "/admin/**", "/authorize/**"))
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(securityProblemSupport)
                         .accessDeniedHandler(securityProblemSupport))
-                .authorizeRequests(req -> req
+                .authorizeRequests(authorizeRequests -> authorizeRequests
                         .antMatchers("/authorize/**").permitAll()
                         .antMatchers("/admin/**").hasRole("ADMIN")
                         .antMatchers("/api/**").hasRole("USER")
                         .anyRequest().authenticated())
-                        .addFilterAt(restAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                        .csrf(crsf -> crsf.ignoringAntMatchers("/authorize/**", "/admin/**", "/api/**"));
-
-                //.formLogin(AbstractHttpConfigurer::disable)
-//                .formLogin(form -> form.loginPage("/login")
-//                            .usernameParameter("username1")
-//                            //.loginProcessingUrl("")
-//                            .successHandler(jsonAuthenticationSuccessHandler())
-//                            .failureHandler(jsonLoginFailureHandler())
-//                            .permitAll())
-//                            //.httpBasic(withDefaults())
-//                            .csrf(withDefaults())
-//                        //.csrf(AbstractHttpConfigurer::disable);
-//                        .logout(longUrl -> longUrl.logoutUrl("/perform_logout")
-//                                .logoutSuccessHandler(((req, res, auth) -> {
-//                                    val objMapper = new ObjectMapper();
-//                                    res.setStatus(HttpStatus.OK.value());
-//                                    res.getWriter().println(objMapper.writeValueAsString(auth));
-//                                    log.debug("logout successfully");
-//
-//                                })))
-//                        .rememberMe(rememberMe -> rememberMe.tokenValiditySeconds(100).rememberMeCookieName("someKeyToRemember"));
-
-
-//        http.csrf(csrf -> csrf.disable() )
-//                .httpBasic(Customizer.withDefaults())
-//                .formLogin(form -> form.disable())
-//                .authorizeRequests(req -> req.antMatchers("/api/**").authenticated());
+                .addFilterAt(restAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .csrf(csrf -> csrf.ignoringAntMatchers("/authorize/**", "/admin/**", "/api/**"))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
     }
 
-    private AuthenticationFailureHandler jsonLoginFailureHandler() {
-        return (req, res, exp) -> {
-            val objMapper = new ObjectMapper();
-            res.setStatus(HttpStatus.UNAUTHORIZED.value());
-            res.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            res.setCharacterEncoding("UTF-8");
-            // final Map<String, String>
-            val errData = Map.of(
-                    "title", "Authentication failed!!!!",
-                    "details", exp.getMessage()
-            );
-            res.getWriter().println(objMapper.writeValueAsString(errData));
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers( "/error/**");
+    }
 
-        };
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("user")
+                .password("{bcrypt}$2a$10$jhS817qUHgOR4uQSoEBRxO58.rZ1dBCmCTjG8PeuQAX4eISf.zowm")
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("old_user")
+                .password("{SHA-1}7ce0359f12857f2a90c7de465f40a95f01cb5da9")
+                .roles("USER");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // 默认编码算法的 Id
+        val idForEncode = "bcrypt";
+        // 要支持的多种编码器
+        val encoders = Map.of(
+                idForEncode, new BCryptPasswordEncoder(),
+                "SHA-1", new MessageDigestPasswordEncoder("SHA-1")
+        );
+        return new DelegatingPasswordEncoder(idForEncode, encoders);
     }
 
     private RestAuthenticationFilter restAuthenticationFilter() throws Exception {
         RestAuthenticationFilter filter = new RestAuthenticationFilter(objectMapper);
         filter.setAuthenticationSuccessHandler(jsonAuthenticationSuccessHandler());
         filter.setAuthenticationFailureHandler(jsonLoginFailureHandler());
-        // authenticationManager from parent
         filter.setAuthenticationManager(authenticationManager());
         filter.setFilterProcessesUrl("/authorize/login");
         return filter;
+    }
+
+    private AuthenticationFailureHandler jsonLoginFailureHandler() {
+        return (req, res, exp) -> {
+            val objectMapper = new ObjectMapper();
+            res.setStatus(HttpStatus.UNAUTHORIZED.value());
+            res.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            res.setCharacterEncoding("UTF-8");
+            val errData = Map.of(
+                    "title", "认证失败",
+                    "details", exp.getMessage()
+            );
+            res.getWriter().println(objectMapper.writeValueAsString(errData));
+        };
+    }
+
+    private LogoutSuccessHandler jsonLogoutSuccessHandler() {
+        return (req, res, auth) -> {
+            if (auth != null && auth.getDetails() != null) {
+                req.getSession().invalidate();
+            }
+            res.setStatus(HttpStatus.OK.value());
+            res.getWriter().println();
+            log.debug("成功退出登录");
+        };
     }
 
     private AuthenticationSuccessHandler jsonAuthenticationSuccessHandler() {
@@ -135,42 +147,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             ObjectMapper objectMapper = new ObjectMapper();
             res.setStatus(HttpStatus.OK.value());
             res.getWriter().println(objectMapper.writeValueAsString(auth));
-            log.debug("success");
+            log.debug("认证成功");
         };
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(passwordEncoder().encode("12345678"))
-                .roles("USER", "ADMIN")
-                .and()
-                .withUser("TestOldEncode")
-                .password(new MessageDigestPasswordEncoder("SHA-1").encode("abcd1234"))
-                .roles("USER", "ADMIN");
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-
-        // support and adopt new encoder
-        val idForDefault = "bcrypt";
-
-        val encoders = Map.of(
-             idForDefault, new BCryptPasswordEncoder(),
-                "SHA-1", new MessageDigestPasswordEncoder("SHA-1")
-        );
-
-        return new DelegatingPasswordEncoder(idForDefault, encoders);
-
-        // Pbkdf2PasswordEncoder()
-        //return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().mvcMatchers("/public/**", "/error")
-        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
